d3fd5799667adf0814f3ed532fceb0c4e73e9a60
Zhadanjun090701 <animalos123454678@gmail.com>
2022-09-07 12:20:19 +0800

    Initial commit

---
+++ scripts/window.js
@@ -0,0 +1,6 @@
+let screenWidth = window.innerWidth
+let screenHeight = window.innerHeight
+
+if (screenWidth <= screenHeight) {
+    alert('Your screen size was not compatible to this site, please try change the device😉')
+}
