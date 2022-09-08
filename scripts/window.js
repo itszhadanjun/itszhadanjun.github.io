5f890a41da3240a6c933d2147e3111fb4bb94465
Zhadanjun090701 <animalos123454678@gmail.com>
2022-09-08 16:48:18 +0800

    显示错误问题修复 Display error fixed

--- scripts/window.js
+++
@@ -1,6 +0,0 @@
-let screenWidth = window.innerWidth;
-let screenHeight = window.innerHeight;
-
-if (screenWidth <= screenHeight) {
-    alert('Your device screen may not compatible to this site, there may have display error');
-}
