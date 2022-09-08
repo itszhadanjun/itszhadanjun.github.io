0c8bb00da3f1a8a0fbddbc043766bc3e7964adb9
Zhadanjun090701 <animalos123454678@gmail.com>
2022-09-08 16:39:03 +0800

    标题栏第一大更新

--- scripts/window.js
+++ scripts/window.js
@@ -1,6 +1,6 @@
 let screenWidth = window.innerWidth;
 let screenHeight = window.innerHeight;
 
-if (screenWidth => screenHeight) {
+if (screenWidth <= screenHeight) {
     alert('Your device screen may not compatible to this site, there may have display error');
 }
